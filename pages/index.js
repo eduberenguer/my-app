72b1ff342b00859f27b6227138ce32bb7c4b0807
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2022-03-04 16:50:35 +0100

    (feat): Add dinamic token

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-const Page = ({ data }) => {
+const Page = ({ data, token }) => {
   console.log(data);
   const deleteAccount = (url) => {
     const id = url.split(`Contact/`)[1];
-    deleteAccountinSalesforce(id);
+    deleteAccountinSalesforce(id, token);
   };
 
   const createUser = async (event) => {
     event.preventDefault();
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append(
-      "Authorization",
-      "Bearer 00D7Q000003RTF1!AQ0AQKOXfPcfPkmHhDMSpfGYnUYH8adI4h4Pf2iLklz2cJHwAS6vjbFyYlLYKlNT3zJmDxypDCib5a71dSQZNW3IqxMtlo9B"
-    );
+    myHeaders.append("Authorization", `Bearer ${token}`);
 
     const raw = JSON.stringify({
       lastName: event.target.name.value,
@@ -41,10 +38,7 @@ const Page = ({ data }) => {
     event.preventDefault();
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append(
-      "Authorization",
-      "Bearer 00D7Q000003RTF1!AQ0AQKOXfPcfPkmHhDMSpfGYnUYH8adI4h4Pf2iLklz2cJHwAS6vjbFyYlLYKlNT3zJmDxypDCib5a71dSQZNW3IqxMtlo9B"
-    );
+    myHeaders.append("Authorization", `Bearer ${token}`);
 
     const raw = JSON.stringify({
       lastName: event.target.name.value,
@@ -73,72 +67,71 @@ const Page = ({ data }) => {
         <input id="name" name="name" type="text" autoComplete="name" required />
         <button type="submit">Create</button>
       </form>
-      {data.records.map((item) => {
-        return (
-          <div key={item.attributes.url}>
-            <p>--------------------</p>
-            <p>{item.Name}</p>
-            <button onClick={() => deleteAccount(item.attributes.url)}>
-              Borrar
-            </button>
-            <form onSubmit={(e) => editUser(e, item.attributes.url)}>
-              <label htmlFor="name">New Name</label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                autoComplete="name"
-                required
-              />
-              <button type="submit">Edit</button>
-            </form>
-          </div>
-        );
-      })}
+      {data &&
+        data.records &&
+        data.records.map((item) => {
+          return (
+            <div key={item.attributes.url}>
+              <p>--------------------</p>
+              <p>{item.Name}</p>
+              <button onClick={() => deleteAccount(item.attributes.url)}>
+                Borrar
+              </button>
+              <form onSubmit={(e) => editUser(e, item.attributes.url)}>
+                <label htmlFor="name">New Name</label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                />
+                <button type="submit">Edit</button>
+              </form>
+            </div>
+          );
+        })}
     </div>
   );
 };
 
 // Método para que me devuelva los datos de la API:
-// const Index = async () => {
-//   const url = "https://login.salesforce.com/services/oauth2/token";
-//   let myHeaders = new Headers();
-//   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-//   myHeaders.append("Accept", "application/json");
+const Index = async () => {
+  const url = "https://login.salesforce.com/services/oauth2/token";
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  myHeaders.append("Accept", "application/json");
 
-//   let urlencoded = new URLSearchParams();
-//   urlencoded.append("grant_type", "password");
-//   urlencoded.append(
-//     "client_id",
-//     "3MVG9t0sl2P.pByqjwU1VYfF7EEJ7rRTcnmB7soewsYGvkKw2ENlww7V_NCbQF11IGKChBC7mmWvllM92TUCX"
-//   );
-//   urlencoded.append(
-//     "client_secret",
-//     "C15623E57349D2FFCAE72BAE8047504DAF6343D1DFF1BF6442D14BA2F260D02C\n"
-//   );
-//   urlencoded.append("username", "eduardo.berenguer@playful-raccoon-kmmjx6.com");
-//   urlencoded.append("password", "Sumadre90ouzrwT5lsyFIGhU0pD4JVIFH");
+  let urlencoded = new URLSearchParams();
+  urlencoded.append("grant_type", "password");
+  urlencoded.append(
+    "client_id",
+    "3MVG9t0sl2P.pByqjwU1VYfF7EEJ7rRTcnmB7soewsYGvkKw2ENlww7V_NCbQF11IGKChBC7mmWvllM92TUCX"
+  );
+  urlencoded.append(
+    "client_secret",
+    "C15623E57349D2FFCAE72BAE8047504DAF6343D1DFF1BF6442D14BA2F260D02C\n"
+  );
+  urlencoded.append("username", "eduardo.berenguer@playful-raccoon-kmmjx6.com");
+  urlencoded.append("password", "Sumadre90ouzrwT5lsyFIGhU0pD4JVIFH");
 
-//   const requestOptions = {
-//     method: "POST",
-//     headers: myHeaders,
-//     body: urlencoded,
-//     redirect: "follow",
-//   };
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
 
-//   await fetch(url, requestOptions)
-//     .then((response) => response.text())
-//     .then((result) => console.log(result))
-//     .catch((error) => console.log("error", error));
-// };
+  return await fetch(url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => JSON.parse(result).access_token)
+    .catch((error) => console.log("error", error));
+};
 
-export const deleteAccountinSalesforce = (id) => {
+export const deleteAccountinSalesforce = (id, token) => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append(
-    "Authorization",
-    "Bearer 00D7Q000003RTF1!AQ0AQKOXfPcfPkmHhDMSpfGYnUYH8adI4h4Pf2iLklz2cJHwAS6vjbFyYlLYKlNT3zJmDxypDCib5a71dSQZNW3IqxMtlo9B"
-  );
+  myHeaders.append("Authorization", `Bearer ${token}`);
 
   const requestOptions = {
     method: "DELETE",
@@ -146,7 +139,7 @@ export const deleteAccountinSalesforce = (id) => {
     redirect: "follow",
   };
 
-  fetch(
+  return fetch(
     `https://playful-raccoon-kmmjx6-dev-ed.my.salesforce.com/services/data/v53.0/sobjects/Contact/${id}`,
     requestOptions
   )
@@ -156,12 +149,10 @@ export const deleteAccountinSalesforce = (id) => {
 };
 
 export const getServerSideProps = async () => {
+  const token = await Index();
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append(
-    "Authorization",
-    "Bearer 00D7Q000003RTF1!AQ0AQKOXfPcfPkmHhDMSpfGYnUYH8adI4h4Pf2iLklz2cJHwAS6vjbFyYlLYKlNT3zJmDxypDCib5a71dSQZNW3IqxMtlo9B"
-  );
+  myHeaders.append("Authorization", `Bearer ${token}`);
 
   const requestOptions = {
     method: "GET",
@@ -176,7 +167,7 @@ export const getServerSideProps = async () => {
 
   const data = await response.json();
 
-  return { props: { data } };
+  return { props: { data, token } };
 };
 
 export default Page;
