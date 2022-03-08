cd0c03172f167c62b890419892ac15a29ac8d5b9
moisesrj97 <moisesrj97@gmail.com>
2022-03-08 10:58:02 +0100

    Add refresh of SSProps when interacting with API

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,32 @@
-import React from "react";
+import { useRouter } from 'next/router';
+import React from 'react';
+import {
+  createContact,
+  deleteContact,
+  editContact,
+  getAllContacts,
+  getToken,
+} from '../services/api-connection';
 
 const Page = ({ data, token }) => {
-  console.log(data);
-  const deleteAccount = (url) => {
+  const router = useRouter();
+  const deleteAccount = async (url) => {
     const id = url.split(`Contact/`)[1];
-    deleteAccountinSalesforce(id, token);
+    await deleteContact(id, token);
   };
 
-  const createUser = async (event) => {
-    event.preventDefault();
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", `Bearer ${token}`);
-
-    const raw = JSON.stringify({
-      lastName: event.target.name.value,
-    });
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://playful-raccoon-kmmjx6-dev-ed.my.salesforce.com/services/data/v53.0/sobjects/Contact/`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
-  };
-
-  const editUser = async (event, url) => {
-    const id = url.split(`Contact/`)[1];
-    event.preventDefault();
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", `Bearer ${token}`);
-
-    const raw = JSON.stringify({
-      lastName: event.target.name.value,
-    });
-
-    const requestOptions = {
-      method: "PATCH",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://playful-raccoon-kmmjx6-dev-ed.my.salesforce.com/services/data/v53.0/sobjects/Contact/${id}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
+  const refreshSSProps = () => {
+    router.replace(router.asPath);
   };
 
   return (
     <div>
-      <form onSubmit={createUser}>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" type="text" autoComplete="name" required />
-        <button type="submit">Create</button>
+      <form
+        onSubmit={(e) => createContact(e, token).then(() => refreshSSProps())}
+      >
+        <label htmlFor='name'>Name</label>
+        <input id='name' name='name' type='text' autoComplete='name' required />
+        <button type='submit'>Create</button>
       </form>
       {data &&
         data.records &&
@@ -74,19 +35,31 @@ const Page = ({ data, token }) => {
             <div key={item.attributes.url}>
               <p>--------------------</p>
               <p>{item.Name}</p>
-              <button onClick={() => deleteAccount(item.attributes.url)}>
+              <button
+                onClick={() =>
+                  deleteAccount(item.attributes.url).then(() =>
+                    refreshSSProps()
+                  )
+                }
+              >
                 Borrar
               </button>
-              <form onSubmit={(e) => editUser(e, item.attributes.url)}>
-                <label htmlFor="name">New Name</label>
+              <form
+                onSubmit={(e) =>
+                  editContact(e, item.attributes.url, token).then(() =>
+                    refreshSSProps()
+                  )
+                }
+              >
+                <label htmlFor='name'>New Name</label>
                 <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
+                  id='name'
+                  name='name'
+                  type='text'
+                  autoComplete='name'
                   required
                 />
-                <button type="submit">Edit</button>
+                <button type='submit'>Edit</button>
               </form>
             </div>
           );
@@ -96,76 +69,10 @@ const Page = ({ data, token }) => {
 };
 
 // Método para que me devuelva los datos de la API:
-const Index = async () => {
-  const url = "https://login.salesforce.com/services/oauth2/token";
-  let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-  myHeaders.append("Accept", "application/json");
-
-  let urlencoded = new URLSearchParams();
-  urlencoded.append("grant_type", "password");
-  urlencoded.append(
-    "client_id",
-    "3MVG9t0sl2P.pByqjwU1VYfF7EEJ7rRTcnmB7soewsYGvkKw2ENlww7V_NCbQF11IGKChBC7mmWvllM92TUCX"
-  );
-  urlencoded.append(
-    "client_secret",
-    "C15623E57349D2FFCAE72BAE8047504DAF6343D1DFF1BF6442D14BA2F260D02C\n"
-  );
-  urlencoded.append("username", "eduardo.berenguer@playful-raccoon-kmmjx6.com");
-  urlencoded.append("password", "Sumadre90ouzrwT5lsyFIGhU0pD4JVIFH");
-
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: "follow",
-  };
-
-  return await fetch(url, requestOptions)
-    .then((response) => response.text())
-    .then((result) => JSON.parse(result).access_token)
-    .catch((error) => console.log("error", error));
-};
-
-export const deleteAccountinSalesforce = (id, token) => {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", `Bearer ${token}`);
-
-  const requestOptions = {
-    method: "DELETE",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
-  return fetch(
-    `https://playful-raccoon-kmmjx6-dev-ed.my.salesforce.com/services/data/v53.0/sobjects/Contact/${id}`,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-};
 
 export const getServerSideProps = async () => {
-  const token = await Index();
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", `Bearer ${token}`);
-
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
-  const response = await fetch(
-    "https://playful-raccoon-kmmjx6-dev-ed.my.salesforce.com/services/data/v53.0/query/?q=SELECT FIELDS(All) FROM Contact LIMIT 200",
-    requestOptions
-  );
-
-  const data = await response.json();
+  const token = await getToken();
+  const data = await getAllContacts();
 
   return { props: { data, token } };
 };
